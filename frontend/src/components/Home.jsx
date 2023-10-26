2e32770a15a5ad36cd3f7b8381cb286d0fb0b993
Thiago Carvalho <thiago099carvalho@gmail.com>
2023-10-26 00:52:44 -0300

    fix: order button

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
     const [newUser, setNewUser] = useState('');
     const [users, setUsers] = useState([]);
     const [error, setError] = useState('');
+    const [sortedUsers, setSorted] = useState([]);
 
     const closeError = () => {
         setError('');
@@ -61,9 +62,9 @@ const Home = () => {
             setUsers((prevUsers) =>
                 prevUsers.map((user) => {
                     if (user.username === username) {
-                        return { ...user, starred: !user.starred };
+                        return { ...user, favorite: !user.favorite };
                     } else {
-                        return { ...user, starred: false };
+                        return { ...user, favorite: false };
                     }
                 })
             );
@@ -73,16 +74,30 @@ const Home = () => {
         }
     };
 
+    const handleOrder = () => {
+      users.sort((a, b) => {
+        const nameA = a.name.toLowerCase();
+        const nameB = b.name.toLowerCase();
+      
+        if (nameA < nameB) return -1;
+        if (nameA > nameB) return 1;
+        return 0;
+      });
+      console.log(users)
+      setSorted(users)
+    
+    }
+
     return (
       
       <div className='bg-primary-dark h-screen justify-center overflow-scroll pb-20'>
           {
             error && (
-              <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-                <strong class="font-bold">Erro :c </strong>
-                <span class="block sm:inline">{error}</span>
-                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                  <svg  onClick={() => closeError()} class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Fechar</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+              <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <strong className="font-bold">Erro :c </strong>Compiled successfully!
+                <span className="block sm:inline">{error}</span>
+                <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                  <svg  onClick={() => closeError()} className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Fechar</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
                 </span>
               </div>
             )
@@ -109,7 +124,15 @@ const Home = () => {
             </div>
           </div>
           <div className='pt-40 sm:w-[80%] md:w-[50%] m-auto'>
-            {users && (
+          <button className='bg-primary-light hover:bg-green text-primary-dark p-2 ml-5 rounded-lg' onClick={() => handleOrder()}>Ordenar por nome</button>
+            {(sortedUsers.length > 0) && 
+              sortedUsers.map((item => (
+                <>                
+                  <UserCard data={item} handleDelete={handleDeleteUser} toggleStar={handleToggleStar} key={item?.username}/>
+                </>
+              )))
+            }
+            {(sortedUsers.length === 0) && (
               users.map((item => (
                 <>                
                   <UserCard data={item} handleDelete={handleDeleteUser} toggleStar={handleToggleStar} key={item?.username}/>
